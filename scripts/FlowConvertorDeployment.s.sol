20ad75a90b863ff7b0bf46f3b39ba133ba1d6035
0xmotto <0xmotto@protonmail.com>
2023-03-08 01:36:59 +0800

    fix: fix FlowConvertorDeployment.s.sol

--- scripts/FlowConvertorDeployment.s.sol
+++ scripts/FlowConvertorDeployment.s.sol
@@ -3,24 +3,25 @@ pragma solidity 0.8.13;
 
 // Scripting tool
 import {Script} from "../lib/forge-std/src/Script.sol";
-
+import {IFlow} from "../contracts/interfaces/IFlow.sol";
 import {FlowConvertor} from "../contracts/FlowConvertor.sol";
 
 contract FlowConvertorDeployment is Script {
     address private constant TEAM_MULTI_SIG = 0x13eeB8EdfF60BbCcB24Ec7Dd5668aa246525Dc51;
     // TODO: Fill the address
-    address private constant FLOW = 0x0000000000000000000000000000000000000000;
+    address private constant FLOW = 0x78e489523291581205Ea3fA16a69689EcA79757A;
+    uint256 private constant FIFTY_MILLION = 50e24; // 50e24 == 50e6 (50m) ** 1e18 (decimals)
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        FlowConvertor flowConvertor = new FlowConvertor({_v1: 0x2baec546a92ca3469f71b7a091f7df61e5569889, _v2: FLOW});
+        FlowConvertor flowConvertor = new FlowConvertor({_v1: 0x2Baec546a92cA3469f71b7A091f7dF61e5569889, _v2: FLOW});
 
         flowConvertor.transferOwnership(TEAM_MULTI_SIG);
 
-        IERC20(FLOW).transfer(address(flowConvertor), 50_000_000e18);
+        IFlow(FLOW).transfer(address(flowConvertor), FIFTY_MILLION);
 
         vm.stopBroadcast();
     }
